7e84063dfeefb664e4264354f225de641141cd5c
Aditya Nashikkar <adityanashikkar94@gmail.com>
2018-08-18 17:53:11 +0530

    Another commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+	constructor(private http: Http) {}
+	httpdata;
+	ngOnInit() {
+		this.http.get("http://jsonplaceholder.typicode.com/users").
+		map((response) => response.json()).
+		subscribe(
+			(data) => { this.displaydata(data); }
+		)
+	}
+	displaydata(data) {this.httpdata = data;}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	
+	declarations: [ 
+		AppComponent 
+	],
+
+	imports: [ 
+		BrowserModule,
+		BrowserAnimationsModule, 
+		HttpModule
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
+
+
 export class AppModule { }
